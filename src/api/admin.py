e17ff333e5569ee19943ca3348ba452ad7ab2ac9
Agustín Demontel <53986476+ademontel@users.noreply.github.com>
2025-02-01 19:39:14 +0000

    Elimina modelos y vistas no utilizados en la API de administración

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Product, Cart, CartItem, Order, OrderItem
+from .models import db, User
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,11 +12,7 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Product, db.session))
-    admin.add_view(ModelView(Cart, db.session))
-    admin.add_view(ModelView(CartItem, db.session))
-    admin.add_view(ModelView(Order, db.session))
-    admin.add_view(ModelView(OrderItem, db.session))
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -12,8 +12,7 @@ class User(db.Model):
     shipping_address = db.Column(db.String(200), nullable=True)
     shipping_phone = db.Column(db.String(120), nullable=True)
     shipping_method = db.Column(db.String(120), nullable=True)
-    cart = db.relationship('Cart', backref='user', lazy=True)
-    orders = db.relationship('Order', backref='user', lazy=True)
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -29,107 +28,3 @@ class User(db.Model):
             "shipping_method": self.shipping_method
         }
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    image = db.Column(db.String(200), nullable=False)
-    stock = db.Column(db.Integer, nullable=True)
-    sku = db.Column(db.String(120), nullable=True)
-    collection = db.Column(db.String(120), nullable=True)
-    genre = db.Column(db.String(120), nullable=True)
-    size = db.Column(db.String(120), nullable=True)
-    color = db.Column(db.String(120), nullable=True)
-    cart_items = db.relationship('CartItem', backref='product', lazy=True)
-    order_items = db.relationship('OrderItem', backref='product', lazy=True)
-
-    def __repr__(self):
-        return f'<Product {self.name}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "price": self.price,
-            "image": self.image,
-            "stock": self.stock,
-            "sku": self.sku,
-            "collection": self.collection,
-            "genre": self.genre,
-            "size": self.size,
-            "color": self.color
-        }
-
-class Cart(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    cart_items = db.relationship('CartItem', backref='cart', lazy=True)
-
-    def __repr__(self):
-        return f'<Cart {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "cart_items": [item.serialize() for item in self.cart_items]
-        }
-
-class CartItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f'<CartItem {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "cart_id": self.cart_id,
-            "product_id": self.product_id,
-            "quantity": self.quantity
-        }
-
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    order_items = db.relationship('OrderItem', backref='order', lazy=True)
-    total = db.Column(db.Float, nullable=False)
-    status = db.Column(db.String(120), nullable=False)
-    payment_method = db.Column(db.String(120), nullable=False)
-    shipping_address = db.Column(db.String(200), nullable=False)
-    shipping_phone = db.Column(db.String(120), nullable=False)
-
-    def __repr__(self):
-        return f'<Order {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "total": self.total,
-            "status": self.status,
-            "payment_method": self.payment_method,
-            "shipping_address": self.shipping_address,
-            "shipping_phone": self.shipping_phone,
-            "order_items": [item.serialize() for item in self.order_items]
-        }
-
-class OrderItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f'<OrderItem {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "order_id": self.order_id,
-            "product_id": self.product_id,
-            "quantity": self.quantity
-        }
